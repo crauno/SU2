088e09b02f1595dd0b0c157b813cad05b090a148
RoccoBombardieri <bombardierirocco@gmail.com>
2021-04-28 15:58:16 +0200

    Adding printing flags to debug segmentation error

--- SU2_CFD/src/solver_direct_elasticity.cpp
+++ SU2_CFD/src/solver_direct_elasticity.cpp
@@ -1982,7 +1982,7 @@ void CFEASolver::BC_Clamped(CGeometry *geometry, CNumerics *numerics, CConfig *c
   Solution[0] = 0.0;
   Solution[1] = 0.0;
   if (nDim==3) Solution[2] = 0.0;
-
+  
   for (iVertex = 0; iVertex < geometry->nVertex[val_marker]; iVertex++) {
 
     /*--- Get node index ---*/
@@ -2018,6 +2018,7 @@ void CFEASolver::BC_Symmetry_y(CGeometry *geometry, CNumerics *numerics, CConfig
   bool dynamic = config->GetTime_Domain();
   su2double zero= 0.0;
   unsigned short vary = 1;
+  cout << "DEBUG 1" << endl;
   for ( iVertex = 0; iVertex < geometry->nVertex[val_marker]; iVertex++) {
 
     /*--- Get node index ---*/
@@ -2025,21 +2026,25 @@ void CFEASolver::BC_Symmetry_y(CGeometry *geometry, CNumerics *numerics, CConfig
 
     /*--- Set and enforce solution at current and previous time-step ---*/
     nodes->SetSolution(iPoint, &zero);
-
+    cout << "DEBUG 2" << endl;
     if (dynamic) {
       nodes->SetSolution_Vel(iPoint,vary, zero);
       nodes->SetSolution_Accel(iPoint,vary, zero); 
       nodes->Set_Solution_time_n(iPoint,vary, zero);
       nodes->SetSolution_Vel_time_n(iPoint,vary, zero);
       nodes->SetSolution_Accel_time_n(iPoint,vary, zero);  
+      cout << "DEBUG 3" << endl;
     }
 
     /*--- Set and enforce 0 solution for mesh deformation ---*/
     nodes->SetBound_Disp(iPoint,vary, zero);
-
+    cout << "DEBUG 4" << endl;
     LinSysSol.SetBlock(iPoint,vary, zero);
+    cout << "DEBUG 5" << endl;
     LinSysReact.SetBlock(iPoint,vary, zero);
+    cout << "DEBUG 6" << endl;
     Jacobian.EnforceDoFSolutionAtNode(iPoint,vary, zero, LinSysRes);
+    cout << "DEBUG 7" << endl;
 
   }
 
